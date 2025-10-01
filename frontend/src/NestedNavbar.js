74f02e10976b8581eb390d57320e9e6648cca27f
pacacodes <pascale.marill@gmail.com>
2025-10-01 20:04:48 +0000

    fix: update navSubtitles in NestedNavbar for improved organization and clarity

--- frontend/src/NestedNavbar.js
+++ frontend/src/NestedNavbar.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { Box, Stack, Text } from '@mantine/core';
 
 const navSubtitles = [
-  [
-    'Users',
+  ['Users',
     'Professionals',
     'Ideas',
     'Budgets',
     'Calendar & Timelines'
   ], // Ideas
-  ['Daily Task 1', 'Daily Task 2'], // Daily Management
-  ['Map Info 1', 'Map Info 2'], // Existing Info
+    ['Daily Task 1', 'Daily Task 2'], // Daily Management
+    [
+      'Worksheets',
+      'Presentation Sheets',
+      'Schedules & Specifications',
+      'Site Diagrams',
+      'Budgets',
+      'Project Files',
+      'Calendar & Timelines'
+    ], // Map
   ['New Project', 'Select Project', 'Import Project', 'Export Project'], // Alpaca
   ['Construction Management 1', 'Construction Management 2'], // HardHat
   ['Design 1', 'Design 2'], // Pen
