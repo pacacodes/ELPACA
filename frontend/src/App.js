524ec6450fdae3f1a543da8954ee9d70832fd056
pacacodes <pascale.marill@gmail.com>
2025-10-10 00:53:40 +0000

    fix: Remove PermaculturePopup and adjust NestedNavbar height for improved layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import Layouts from './NestedNavbar';
 import CommunicationNavbar from './CommunicationNavbar';
 import ViewsSection from './ViewsSection';
 import ObjectsAndSystemsServiceButton from './ObjectsAndSystemsServiceButton';
-// import PermaculturePopup from './PermaculturePopup';
 import { AppShell } from '@mantine/core';
 import BIMApp from './bim/BIMApp';
 
@@ -27,8 +26,6 @@ function App() {
   <CommunicationNavbar />
   {/* Views section below Communication */}
   <ViewsSection />
-  {/* PermaculturePopup removed/commented out */}
-  {/* <PermaculturePopup /> */}
     <ObjectsAndSystemsServiceButton />
   <AppShell.Main style={{ height: '100vh', overflow: 'hidden', padding: 0, position: 'absolute', top: 0, left: 0, width: '100%' }}>
         {/* Render main content based on navigation/layout selection */}
