2eded444ff718232006b0c4bf10b7f8147b9b5d0
pacacodes <pascale.marill@gmail.com>
2025-10-10 01:06:21 +0000

    fix: Adjust positioning of communication navbar and remove PermaculturePopup for improved layout

--- frontend/src/CommunicationNavbar.js
+++ frontend/src/CommunicationNavbar.js
@@ -13,7 +13,7 @@ const communicationLinks = [
 
 export default function CommunicationNavbar() {
   return (
-  <Box className="communication-navbar-scroll" style={{ position: 'fixed', top: 630, right: 29, width: 180, minWidth: 180, maxWidth: 340, height: '270px', zIndex: 198, background: 'rgba(245,245,245,0.5)', borderRadius: 8, padding: 16, overflowY: 'auto' }}>
+  <Box className="communication-navbar-scroll" style={{ position: 'fixed', top: 590, right: 29, width: 180, minWidth: 180, maxWidth: 340, height: '270px', zIndex: 198, background: 'rgba(245,245,245,0.5)', borderRadius: 8, padding: 16, overflowY: 'auto' }}>
       <Text
         fw={400}
         c="#23272A"
