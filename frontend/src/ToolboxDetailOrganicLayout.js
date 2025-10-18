b1453c3dbd5b30eafc332bdb50e7afcf0cb9dfdf
pacacodes <pascale.marill@gmail.com>
2025-10-18 21:06:33 +0000

    fix: enhance layout and styling in ToolboxDetailOrganicLayout for improved visual presentation

--- frontend/src/ToolboxDetailOrganicLayout.js
+++ frontend/src/ToolboxDetailOrganicLayout.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Grid, Card, Text, Group } from '@mantine/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTree } from '@fortawesome/free-solid-svg-icons';
 import SunSection from './ToolboxDetailOrganicSunSection';
 import WateringSection from './ToolboxDetailOrganicWateringSection';
 import SoilTypeSection from './ToolboxDetailOrganicSoilTypeSection';
 
 export default function ToolboxDetailOrganicLayout() {
   return (
-    <div style={{ width: 'calc(100% + 100px)', marginTop: 16 }}> {/* Increased width by 100px */}
+    <div style={{ width: 'calc(100% + 100px)', marginTop: 16, display: 'flex', flexDirection: 'column', alignItems: 'center' }}> {/* Centered content */}
+      <div style={{ marginBottom: 16, padding: 8, background: 'rgba(255, 255, 255, 0.05)', borderRadius: 8, maxWidth: 'calc(80% - 100px)', wordWrap: 'break-word', overflowWrap: 'break-word', textAlign: 'center' }}> {/* Centered text box */}
+        <Group position="center"> {/* Centered icons */}
+          <FontAwesomeIcon icon={faTree} style={{ fontSize: '1.2em', color: '#00CED1' }} />
+          <FontAwesomeIcon icon={faTree} style={{ fontSize: '0.88em', marginLeft: '-0.6em', marginTop: '0.32em', color: 'rgba(255, 255, 255, 0.2)' }} />
+        </Group>
+        <Text fw={400} c="#ffffff" style={{ fontSize: '0.90rem', fontFamily: 'inherit', letterSpacing: 1, whiteSpace: 'pre-wrap', wordWrap: 'break-word', lineHeight: 1.5, marginTop: 8 }}> {/* Centered text */}
+          Canopy refers to the upper layer of vegetation in a forest, formed by mature tree crowns.
+        </Text>
+      </div>
       <SunSection style={{ width: 'calc(100% + 100px)' }} /> {/* Increased section width by 100px */}
       <WateringSection style={{ width: 'calc(100% + 100px)' }} /> {/* Increased section width by 100px */}
       <SoilTypeSection style={{ width: 'calc(100% + 100px)' }} /> {/* Increased section width by 100px */}
